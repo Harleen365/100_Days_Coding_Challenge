667902c72211154b135160f38da35840eabe15ec
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-01-22 21:26:18 +0530

    Add files via upload

---
+++ 3sum.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+	// Write your code here.
+    vector<vector<int>>ans;
+    set<vector<int>>temp;
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<n-2;i++){
+        int l=i+1;
+        int r=n-1;
+        while(l<r){
+            if(arr[i]+arr[l]+arr[r]==K){
+                temp.insert({arr[i],arr[l],arr[r]});
+            }
+            if(arr[i]+arr[l]+arr[r]>K){
+
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+
+
+    }
+    for(auto i:temp){
+        ans.push_back(i);
+    }
+    return ans;
+}
