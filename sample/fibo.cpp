3f92a8014760afd0a40ee59871df12fd5a652524
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-02-13 21:01:14 +0530

    Create fibo.cpp

---
+++ sample/fibo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int t1=0;
+int t2=1;
+int next=0;
+for(int i=1;i<=n;i++){
+    if(n==1){
+        cout<<t1<<",";
+        continue;
+    }
+    if(n==2){
+        cout<<t2<<",";
+        continue;
+    }
+    next=t1+t2;
+    t1=t2;
+    t2=next;
+
+    cout<<next<<",";
+}
+}
