25d1d3b4401f0ab5c28f52e2825c86395e5cbec6
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-02-13 21:39:41 +0530

    Add files via upload

---
+++ Day 15/sq_of_sorted array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            int number=nums[i];
+            ans.push_back(number*number);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
