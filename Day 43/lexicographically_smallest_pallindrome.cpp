2c6b76de7ac283f2dffacf8b6a71044378b153bd
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-03-01 21:19:04 +0530

    Create lexicographically_smallest_pallindrome.cpp

---
+++ Day 43/lexicographically_smallest_pallindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                s[i]=s[j]=min(s[i],s[j]);
+            }
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
