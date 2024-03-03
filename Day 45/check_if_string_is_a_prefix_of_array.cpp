4d1fb1adffb52758fd985f8f1668db8ff35a5415
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-03-03 21:11:55 +0530

    Create check_if_string_is_a_prefix_of_array.cpp

---
+++ Day 45/check_if_string_is_a_prefix_of_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        int n=words.size();
+        string res="";
+        for(int i=0;i<n;i++){
+            res+=words[i];
+            if(res==s){
+                return true;
+            }
+            if(res.length()>s.length()){
+                return false;
+            }
+        }
+        return false;
+    }
+};
