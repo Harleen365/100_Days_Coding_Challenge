332bed42be908ca3f801d268035a4f06d80a1c88
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-03-06 21:27:52 +0530

    Create merge_k_sorted_list.cpp

---
+++ Day 48/merge_k_sorted_list.cpp
@@ -0,0 +1,29 @@
+class mycompare{
+    public:
+    bool operator()(const ListNode*a,const ListNode*b){
+        return (a->val>b->val);
+    }
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode*head=new ListNode(0);
+        ListNode*temp=head;
+        priority_queue<ListNode*,vector<ListNode*>,mycompare>pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=NULL){
+                pq.push(lists[i]);
+            }
+        }
+        while(!pq.empty()){
+            auto least=pq.top();
+            pq.pop();
+            temp->next=least;
+            temp=temp->next;
+            if(least->next){
+                pq.push(least->next);
+            }
+        }
+        return head->next;
+    }
+};
