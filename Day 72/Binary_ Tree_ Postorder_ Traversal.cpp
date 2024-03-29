afa95246d22a38794b37d4830ba7c317172e6ec7
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-03-29 22:18:38 +0530

    Create 	Binary_ Tree_ Postorder_ Traversal.cpp

---
+++ Day 72/Binary_ Tree_ Postorder_ Traversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void postOrder(TreeNode* root, vector<int> &ans) {
+        if(!root) return;
+        postOrder(root->left, ans);
+        postOrder(root->right, ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postOrder(root, ans);
+        return ans;
+    }
+};
