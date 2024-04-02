69e5d0cd693899d833887a2cae1816a8e91f4dce
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-04-02 20:32:46 +0530

    Create Balanced_Binary_Tree.cpp

---
+++ Day 75/Balanced_Binary_Tree.cpp
@@ -0,0 +1,52 @@
+// class Solution {
+// public:
+//     int height(TreeNode*root){
+//         if(root==NULL){
+//             return 0;
+//         }
+//         int left=height(root->left);
+//         int right=height(root->right);
+//         return max(left,right)+1;
+//     }
+//     bool isBalanced(TreeNode* root) {
+//         if(root==NULL){
+//             return true;
+//         }
+//         bool left=isBalanced(root->left);
+//         bool right=isBalanced(root->right);
+//         bool diff=abs(height(root->left)-height(root->right))<=1;
+//         if(left && right && diff){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// };//T.C=O(N^2)
+
+//optimised approach
+class Solution {
+public:
+
+    int height(TreeNode*root){
+        if(root==NULL){
+            return 0;
+        }
+        int left=height(root->left);
+        int right=height(root->right);
+        if(left==-1 || right==-1){
+            return -1;
+        }
+        if(abs(left-right)>1){
+        return -1;
+       }
+       return max(left,right)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+      return height(root)!=-1;
+    }
+};
+//T.C :O(N)
+
+
+
