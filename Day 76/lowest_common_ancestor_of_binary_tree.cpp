ce1d39b985dedcffaa1787b8b11aa32e6cb19d1f
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-04-03 20:43:56 +0530

    Create lowest_common_ancestor_of_binary_tree.cpp

---
+++ Day 76/lowest_common_ancestor_of_binary_tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root==p || root==q){
+            return root;
+        }
+        TreeNode*left=lowestCommonAncestor(root->left,p,q);
+        TreeNode*right=lowestCommonAncestor(root->right,p,q);
+
+        if(!left){
+            return right;
+        }
+        else if(!right){
+            return left;
+        }
+        else{
+            return root;
+        }
+    }
+};
