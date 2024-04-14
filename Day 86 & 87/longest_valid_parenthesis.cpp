a10bbd8da49c99663bde05c73e4300b83ec786fd
Harleen365 <127092049+Harleen365@users.noreply.github.com>
2024-04-14 11:31:18 +0530

    Create longest_valid_parenthesis.cpp

---
+++ Day 86 & 87/longest_valid_parenthesis.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int maxlen=0;
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    maxlen=max(maxlen,i-st.top());
+                }
+            }
+        }
+        return maxlen;
+    }
+};
